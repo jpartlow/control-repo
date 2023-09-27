6fc833639dabf63fc9c5302541950c8a29b5c9dc
Josh Partlow <joshua.partlow@puppetlabs.com>
2023-09-27 19:54:58 +0000

    (test) Use a custom config version script
    ...that gets the sha of both a hieradata and a production environment.

---
+++ scripts/code_manager_config_version.rb
@@ -0,0 +1,17 @@
+require 'json'
+require 'socket'
+
+#Get Hostname of puppetmaster
+compile_master = Socket.gethostname
+environmentpath = ARGV[0]
+environment   = ARGV[1]
+
+#output the sha1 from the control-repo
+r10k_deploy_file_path = File.join(environmentpath, environment, '.r10k-deploy.json')
+commit_sha = JSON.parse(File.read(r10k_deploy_file_path))['signature']
+
+hieradata_file_path = File.join(environmentpath, 'hieradata', '.r10k-deploy.json')
+hiera_sha = JSON.parse(File.read(hieradata_file_path))['signature']
+
+#ouput config version
+puts "#{compile_master}/#{environment}::#{commit_sha[0...7]}/hieradata::#{hiera_sha[0...7]}"
